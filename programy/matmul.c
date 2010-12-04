9e5c09e7f2d23a8516fb04f180af0149686dfd96
Hua Xu <xuhua_alex@hotmail.com>
2010-12-04 13:43:47 -0500

    Profiler add in program. Don't compile for SESC

--- programy/matmul.c
+++ programy/matmul.c
@@ -2,19 +2,39 @@
 // Young Jin Yoon <youngjin@cs.columbia.edu>
 
 #include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
 #include "define.h"
 
+/*
+ * This helper function printout the CPU time in X86/X64 structure.
+ * Comment them after optimization
+ */
+#define rdtsc(low,high) \
+	__asm__ __volatile__("rdtsc" : "=a" (low), "=d" (high))
+
+static inline long long int getcycles()
+{
+	unsigned long low;
+	long high;
+	rdtsc(low, high);
+	return ((long long int)high << 32) | low;
+}
+
 double **matmul(double ***A, int *a, int mat_num)
 {
+	/*Cycle time holder*/
+	long long int time_start;
 	/* The Chain Matrix Multiplication STARTS HERE */
 	double ***B;
 	double **result;
 	int i, j, k, l;
+	time_start = getcycles();
 	/* memory allocation for intermediate results */ 
 	B = (double ***) malloc((mat_num)*sizeof(double **));
 	B[0] = A[0];
-	for(i=1; i<mat_num;i++)
+	
+       	for(i=1; i<mat_num;i++)
 	{
 		B[i] = (double **)malloc(a[0]*sizeof(double *));
 		for(j=0; j<a[0]; j++)
@@ -39,7 +59,7 @@ double **matmul(double ***A, int *a, int mat_num)
 		}
 	}
 	result = B[mat_num-1];
-
+	printf("CPU cycles: %lld\n",(getcycles()-time_start));
 	/* memory deallocation - this loop does not deallocate B[0](= A[0]) and B[mat_num](=result).*/
 	for(i=1; i<mat_num-1;i++)
 	{
